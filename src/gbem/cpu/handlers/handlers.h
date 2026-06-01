97d9519b16223ca339ab67d48c92c8d44712aa1c
Taylor Aldridge <aldridge764@gmail.com>
2026-06-01 23:36:07 +0000

    Update tracking sheet with data for all OPCODE handlers

--- src/gbem/cpu/handlers/handlers.h
+++ src/gbem/cpu/handlers/handlers.h
@@ -17,6 +17,21 @@ uint8_t gb_handler_adc_r8(gb_t *gb);
 
 //ld/rd
 uint8_t gb_handler_ld_r8_r8(gb_t *gb);
+uint8_t gb_handler_ld_r8_n8(gb_t *gb);
+uint8_t gb_handler_ld_r16_n16(gb_t *gb);
+uint8_t gb_handler_ld_r16mem_a(gb_t *gb);
+uint8_t gb_handler_ld_a_r16mem(gb_t *gb);
+uint8_t gb_handler_ld_n16mem_sp(gb_t *gb);
+uint8_t gb_handler_ldh_c_a(gb_t *gb);
+uint8_t gb_handler_ldh_n8_a(gb_t *gb);
+uint8_t gb_handler_ld_n16mem_a(gb_t *gb);
+uint8_t gb_handler_ldh_a_c(gb_t *gb);
+uint8_t gb_handler_ldh_a_n8(gb_t *gb);
+uint8_t gb_handler_ld_a_n16mem(gb_t *gb);
+uint8_t gb_handler_ld_hl_sp_e8(gb_t *gb);
+uint8_t gb_handler_ld_sp_hl(gb_t *gb);
+uint8_t gb_handler_pop_r16(gb_t *gb);
+uint8_t gb_handler_push_r16(gb_t *gb);
 
 //control
 
